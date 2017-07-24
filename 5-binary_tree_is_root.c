5fed3f6d288b0d451080880f40a6e81f2e6cc1c8
Carrie Ybay <carrieybay@gmail.com>
2017-07-24 16:46:24 -0700

    Initial commit

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,1 +1,17 @@
-#!/usr/bin/
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if a given node is a root
+ * @node: pointer to node to check
+ * Return: 1 if node is a root, 0 if otherwise or NULL
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
+}
