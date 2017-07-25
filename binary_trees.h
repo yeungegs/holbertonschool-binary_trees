6cb28589d753f8452be1adfc33fb2bfb37efd06e
Carrie Ybay <carrieybay@gmail.com>
2017-07-25 09:49:39 -0700

    Initial WIP commit

--- binary_trees.h
+++ binary_trees.h
@@ -26,7 +26,6 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -46,5 +45,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+void binary_tree_print(const binary_tree_t *);
+size_t measure_height(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_ */
