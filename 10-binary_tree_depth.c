64d24c54308977e4a86d3c0f26c2af152b02ada3
Carrie Ybay <carrieybay@gmail.com>
2017-07-25 10:17:41 -0700

    Remove shebangs

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,1 +1,11 @@
-#!/usr/bin/
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @node: pointer to the node to measure the depth of
+ * Return: depth of the node or 0 if node is NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+
+}
