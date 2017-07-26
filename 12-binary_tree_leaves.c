b9aae7f424d858f682fa5505c7abb47dc5f27f5b
Carrie Ybay <carrieybay@gmail.com>
2017-07-25 17:27:29 -0700

    Refactor code

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,10 +7,14 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t count;
+
 	if (tree == NULL)
 		return (0);
 
-	return (count_leaves(tree));
+	count = count_leaves(tree);
+
+	return (count);
 }
 
 /**
