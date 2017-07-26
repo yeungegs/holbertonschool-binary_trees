7b6771b16073c3ed2c308bb0be6f33f9d7d50f9d
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-07-25 17:44:18 -0700

    Add Task 8 - v1 :tada:

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,1 +1,21 @@
+#include "binary_trees.h"
 
+/**
+ * binary_tree_postorder - function that implements post-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node.
+ * The value in the node must be passed as a parameter to this function.
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (func == NULL)
+		return;
+
+	if (tree == NULL)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
