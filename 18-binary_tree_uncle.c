1e81bada677e83431a6ef59b32bb35b546800ae4
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-07-26 21:29:08 -0700

    Add Task 18 - work in progress - output not matching expected

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,1 +1,28 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - function that finds the uncle of a node
+ * @node: pointer to the node to find the uncle of
+ * Return: pointer to uncle node or NULL
+ */
 
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+	
+	if (node->parent == NULL)
+		return (NULL);
+	
+	if (node->parent->parent == NULL)
+		return (NULL);
+	
+	if (node->parent->parent->right != NULL ||
+	    node->parent->parent->right != node->parent)
+		return (node->parent->parent->right);
+	
+	else if (node->parent->parent->left != NULL ||
+		 node->parent->parent->left != node->parent)
+		return (node->parent->parent->left);
+
+	return (NULL);
+}
