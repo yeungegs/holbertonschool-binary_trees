118be2a6f547b7addc03436d5ca08765ffdb4b7d
Carrie Ybay <carrieybay@gmail.com>
2017-07-27 12:02:42 -0700

    Update to give the correct output

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,8 +7,8 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_sub;
-	int right_sub;
+	int left_height;
+	int right_height;
 
 	if (tree == NULL)
 		return (0);
@@ -17,13 +17,56 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree->left != NULL && tree->right == NULL)
 		return (0);
 
-	left_sub = binary_tree_is_perfect(tree->left);
-	right_sub = binary_tree_is_perfect(tree->right);
+	left_height = measure_height(tree->left);
+	right_height = measure_height(tree->right);
 
-	if (left_sub == 0 && right_sub == 0)
+	if (left_height != right_height)
 		return (0);
-	else if (left_sub == right_sub)
+
+	if (check_children(tree) == 1)
 		return (1);
 	else
 		return (0);
+}
+
+/**
+ * measure_height - measures height of a binary tree recursively
+ * @tree: pointer to the root of the tree to measure
+ * Return: height
+ */
+size_t measure_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = measure_height(tree->left);
+	right_height = measure_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
+
+/**
+ * check_children - checks if a node has two children
+ * @node: pointer to the node to check
+ * Return: 1 if node has two children, 0 if not
+ */
+int check_children(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (1);
+
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+
+	if (node->left != NULL && node->right != NULL)
+		return (check_children(node->left) &&
+			check_children(node->right));
+
+	return (0);
 }
