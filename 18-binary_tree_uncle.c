1506cda3e9d7ba17fc52035ae58d0bca50d088c0
Carrie Ybay <carrieybay@gmail.com>
2017-07-27 13:41:55 -0700

    Task 18 - WIP :construction:

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,18 +9,24 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL)
 		return (NULL);
-	
+
 	if (node->parent == NULL)
 		return (NULL);
-	
+
 	if (node->parent->parent == NULL)
 		return (NULL);
-	
+
+        if (node->parent->parent->left != NULL ||
+		 node->parent->parent->left != node->parent)
+		return (node->parent->parent->left);
+	else if (node->parent->right != NULL ||
+		 node->parent->parent->right != node->parent)
+		return (node->parent->parent->right);
+
 	if (node->parent->parent->right != NULL ||
 	    node->parent->parent->right != node->parent)
 		return (node->parent->parent->right);
-	
-	else if (node->parent->parent->left != NULL ||
+	else if (node->parent->left != NULL ||
 		 node->parent->parent->left != node->parent)
 		return (node->parent->parent->left);
 
