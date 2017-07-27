7ba4d2b630ba341918f9a26784086a6cd276d06e
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-07-26 19:20:35 -0700

    Add Task 16 - work in progress :construction:

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
- * Return: 1 if full, 0 if not full or if tree is NULL
+ * Return: 1 if perfect, 0 if not perfect or if tree is NULL
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
@@ -12,8 +12,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
 	if (tree->left == NULL && tree->right != NULL)
 		return (0);
 	if (tree->left != NULL && tree->right == NULL)
@@ -22,9 +20,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	left_sub = binary_tree_is_perfect(tree->left);
 	right_sub = binary_tree_is_perfect(tree->right);
 
-	if (left_sub == 0 || right_sub == 0)
+	if (left_sub == 0 && right_sub == 0)
 		return (0);
-
 	else if (left_sub == right_sub)
 		return (1);
 	else
