1ce7220862cbe6846bffde4e6ae9f126a6398360
Carrie Ybay <carrieybay@gmail.com>
2017-07-27 13:50:25 -0700

    Update Task 18 - change if statements from OR to AND

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -16,17 +16,11 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node->parent->parent == NULL)
 		return (NULL);
 
-        if (node->parent->parent->left != NULL ||
-		 node->parent->parent->left != node->parent)
-		return (node->parent->parent->left);
-	else if (node->parent->right != NULL ||
-		 node->parent->parent->right != node->parent)
-		return (node->parent->parent->right);
-
-	if (node->parent->parent->right != NULL ||
+	if (node->parent->parent->right != NULL &&
 	    node->parent->parent->right != node->parent)
 		return (node->parent->parent->right);
-	else if (node->parent->left != NULL ||
+
+	if (node->parent->parent->left != NULL &&
 		 node->parent->parent->left != node->parent)
 		return (node->parent->parent->left);
 
